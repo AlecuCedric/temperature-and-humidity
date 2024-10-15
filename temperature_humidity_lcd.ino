f0db689522628e30f10044c03b23836369982015
Cedric <alecucedric@gmail.com>
2024-10-15 16:05:53 +0300

    final commit

--- temperature_humidity_lcd.ino
+++ temperature_humidity_lcd.ino
@@ -1,6 +1,14 @@
 #include <LiquidCrystal_I2C.h>
+#include "dht.h"
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+#define dht_apin A0 // Analog Pin sensor is connected to
+dht DHT;
 
 void setup() {
   lcd.init();         // initialize the lcd
@@ -8,11 +16,28 @@ void setup() {
 }
 
 void loop() {
-  lcd.setCursor(0, 0);
-  lcd.print("Hello");
-  lcd.setCursor(0, 1);
-  lcd.print("World");
+  //read sensor data
+  DHT.read11(dht_apin);
 
+  //output temperature data on the display
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature:");
+  lcd.setCursor(0, 1);
+  lcd.print(DHT.temperature);
+  lcd.print("C");
+
+  //Wait 5 seconds before displaying the humidity
+  delay(5000);
+  lcd.clear();
+
+  //output humidity data on the display
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity:");
+  lcd.setCursor(0, 1);
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+
+  //Wait 5 seconds before accessing sensor again.
   delay(5000);
 }
 
